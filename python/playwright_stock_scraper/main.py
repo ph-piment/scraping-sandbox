bd1ae067787b5b5d2a77f02ef8c8a6e8659691c3
中田 <38421508+ph-piment@users.noreply.github.com>
2025-07-05 00:28:32 +0900

    fix main (#32)
    fix main

--- python/playwright_stock_scraper/main.py
+++ python/playwright_stock_scraper/main.py
@@ -1,10 +1,13 @@
 import asyncio
+import os
 import sys
 
 from playwright.async_api import Error
 
 from playwright_stock_scraper.scraper import scrape
 from playwright_stock_scraper.writer import save_csv, save_json
+
+OUTPUT_DIR_PATH = "outputs/playwright_stock_scraper"
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -13,13 +16,21 @@ if __name__ == "__main__":
 
     symbol = sys.argv[1]
     output_format = sys.argv[2] if len(sys.argv) > 2 else "csv"
-    OUTPUT_PATH = "outputs/playwright_stock_scraper"
+
+    os.makedirs(OUTPUT_DIR_PATH, exist_ok=True)
 
     try:
         data = asyncio.run(scrape(symbol))
+        OUTPUT_FILE_PATH = f"{OUTPUT_DIR_PATH}/{symbol.upper()}.{output_format}"
+
         if output_format == "json":
-            save_json(data, f"{OUTPUT_PATH}/{symbol.upper()}.{output_format}")
+            save_json(data, OUTPUT_FILE_PATH)
+        elif output_format == "csv":
+            save_csv(data, OUTPUT_FILE_PATH)
         else:
-            save_csv(data, f"{OUTPUT_PATH}/{symbol.upper()}.csv")
+            print("❌ Invalid format. Use 'csv' or 'json'.")
+            sys.exit(1)
+
+        print(f"✅ Data saved to {OUTPUT_FILE_PATH}")
     except Error as e:
         print(f"❌ Playwright Error: {e}")
