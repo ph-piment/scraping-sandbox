eb1f90d3fd994487162bcc86c5725e14e0bd6c6b
Takuji.Nakada <ph.piment@gmail.com>
2025-07-05 00:22:03 +0900

    fix

--- python/playwright_stock_scraper/main.py
+++ python/playwright_stock_scraper/main.py
@@ -7,7 +7,7 @@ from playwright.async_api import Error
 from playwright_stock_scraper.scraper import scrape
 from playwright_stock_scraper.writer import save_csv, save_json
 
-OUTPUT_PATH = "outputs/playwright_stock_scraper"
+OUTPUT_DIR_PATH = "outputs/playwright_stock_scraper"
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -17,20 +17,20 @@ if __name__ == "__main__":
     symbol = sys.argv[1]
     output_format = sys.argv[2] if len(sys.argv) > 2 else "csv"
 
-    os.makedirs(OUTPUT_PATH, exist_ok=True)
+    os.makedirs(OUTPUT_DIR_PATH, exist_ok=True)
 
     try:
         data = asyncio.run(scrape(symbol))
-        filename = f"{OUTPUT_PATH}/{symbol.upper()}.{output_format}"
+        OUTPUT_FILE_PATH = f"{OUTPUT_DIR_PATH}/{symbol.upper()}.{output_format}"
 
         if output_format == "json":
-            save_json(data, filename)
+            save_json(data, OUTPUT_FILE_PATH)
         elif output_format == "csv":
-            save_csv(data, filename)
+            save_csv(data, OUTPUT_FILE_PATH)
         else:
             print("❌ Invalid format. Use 'csv' or 'json'.")
             sys.exit(1)
 
-        print(f"✅ Data saved to {filename}")
+        print(f"✅ Data saved to {OUTPUT_DIR_FILE}")
     except Error as e:
         print(f"❌ Playwright Error: {e}")
